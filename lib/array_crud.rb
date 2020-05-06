7e120481a80e56aea62f936d5e825dba98d14c6a
Vianka Rivera <viankalrivera@gmail.com>
2020-05-06 17:12:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,8 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  my_array = ["Art","Music","Food","Vibes"]
-  my_array.unshift("Friends")
+  add_element_to_start_of_array= ["Art","Music","Food","Vibes"]
+  add_element_to_start_of_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
