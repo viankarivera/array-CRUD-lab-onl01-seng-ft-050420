566e4c6f785c1a6b882232885f5fb29fd50b03a9
Vianka Rivera <viankalrivera@gmail.com>
2020-05-06 17:14:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   remove_element_from_end_of_array = ["Art","Music","Food","Vibes"]
-  my_array.pop("Vibes")
+  remove_element_from_end_of_array.pop
 end
 
 def remove_element_from_start_of_array(array)
