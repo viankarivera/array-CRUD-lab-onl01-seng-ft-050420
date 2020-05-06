cc48e7d8a82290d2414d1e395ba15b3dc947aa79
Vianka Rivera <viankalrivera@gmail.com>
2020-05-06 16:56:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,6 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   my_array = ["Art","Music","Food","Vibes"]
   my_array << "Camping"
+  puts my_array.inspect
 end
 
 def add_element_to_start_of_array(array, element)
