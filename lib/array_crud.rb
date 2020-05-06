3ad6d89160b5dff917b329011c7552a35a0892d0
Vianka Rivera <viankalrivera@gmail.com>
2020-05-06 17:18:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   retrieve_element_from_index = ["Art","Music","Food","Vibes"]
-  retrieve_element_from_index[am]
+  retrieve_element_from_index[2]
 end
 
 def retrieve_first_element_from_array(array)
