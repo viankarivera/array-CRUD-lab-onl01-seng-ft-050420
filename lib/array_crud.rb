4a6f0a12caa193f5d58bcb43cee83e671148eebc
Vianka Rivera <viankalrivera@gmail.com>
2020-05-06 17:21:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  retrieve_first_element_from_array = ["Art","Music","Food","Vibes"]
+  retrieve_first_element_from_array = ["wow","Music","Food","Vibes"]
   retrieve_first_element_from_array[0]
 end
 
