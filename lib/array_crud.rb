43d2688f41afb5208658a21410a0f492b19de061
Vianka Rivera <viankalrivera@gmail.com>
2020-05-06 17:21:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,6 +37,6 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  my_array = ["Art","Music","Food","Vibes"]
-  my_array[-1]
+  retrieve_last_element_from_array= ["Art","Music","Food","arrays!"]
+  retrieve_last_element_from_array[-1]
 end
