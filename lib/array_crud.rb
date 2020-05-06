947fbf6c75bd1f5e66d418fdd18f2bde175e14a3
Vianka Rivera <viankalrivera@gmail.com>
2020-05-06 17:19:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
   remove_element_from_start_of_array = ["Art","Music","Food","Vibes"]
-  remove_element_from_start_of_array("Art")
+  remove_element_from_start_of_array.shift("Art")
 end
 
 def retrieve_element_from_index(array, index_number)
